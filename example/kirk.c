d7143bc221f42dfbcc76c466fe64690c91fa4a4a
Tera <24725862+teraa@users.noreply.github.com>
2021-03-16 18:02:38 +0100

    initial commit

---
+++ example/kirk.c
@@ -0,0 +1,44 @@
+/*
+** kirk.c -- writes to a message queue
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+
+struct my_msgbuf {
+    long mtype;
+    char mtext[200];
+};
+
+int main(void)
+{
+    struct my_msgbuf buf;
+    int msqid;
+    key_t key;
+    char text[]="Kirk: We are attacked. Spock, send reinforcement.";
+
+    key = 12345;
+    
+    if ((msqid = msgget(key, 0600 | IPC_CREAT)) == -1) {
+        perror("msgget");
+        exit(1);
+    }
+    
+    memcpy(buf.mtext, text, strlen(text)+1);
+    buf.mtype = 1;
+    
+    if (msgsnd(msqid, (struct msgbuf *)&buf, strlen(text)+1, 0) == -1)
+            perror("msgsnd");
+    
+    printf("Kirk: Reported attacks to Spock, he will send help!\n");
+    
+    return 0;
+}
+
+
+
---
+++ example/spock.c
@@ -0,0 +1,55 @@
+/*
+** spock.c -- reads from a message queue
+*/
+#define _XOPEN_SOURCE 500
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <signal.h>
+
+struct my_msgbuf {
+    long mtype;
+    char mtext[200];
+};
+
+int msqid;
+
+void retreat(int failure) 
+{
+    if (msgctl(msqid, IPC_RMID, NULL) == -1) {
+        perror("msgctl");
+        exit(1);
+    }
+    exit(0);
+}
+
+int main(void)
+{
+    struct my_msgbuf buf;
+    key_t key;
+
+    key = 12345;
+    
+    if ((msqid = msgget(key, 0600 | IPC_CREAT)) == -1) { /* connect to the queue */
+        perror("msgget");
+        exit(1);
+    }
+    
+    sigset(SIGINT, retreat);
+    
+    for(;;) { /* Spock never quits to his captain! */
+        if (msgrcv(msqid, (struct msgbuf *)&buf, sizeof(buf)-sizeof(long), 0, 0) == -1) {
+            perror("msgrcv");
+            exit(1);
+        }
+        printf("Spock: received: \"%s\". \nSending reinforcement!\n", buf.mtext);
+    }
+
+    return 0;
+}
+
+
+
