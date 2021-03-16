6a9c4f782b2a3d24edbe566af6f032a1a0c8bc51
Tera <24725862+teraa@users.noreply.github.com>
2021-03-17 00:34:22 +0100

    oop

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Runtime.InteropServices;
 using System.Text;
 
@@ -6,76 +6,34 @@ namespace NOS.Lab1
 {
     class Program
     {
-        const string DLL_NAME = "../shared/msg.so";
-        const int IPC_CREAT = 0x200;
-        const int IPC_EXCL = 0x400;
-        const int IPC_NOWAIT = 0x800;
-        const int IPC_RMID = 0;
-        const int IPC_SET = 1;
-        const int IPC_STAT = 2;
-
-        [DllImport(DLL_NAME)] static extern void print(string message);
-        [DllImport(DLL_NAME)] static extern void test_struct(ref Message value);
-        [DllImport(DLL_NAME)] static extern void test_chars(string text);
-
-        [DllImport(DLL_NAME)] static extern int msgget(int key, int flags);
-        [DllImport(DLL_NAME)] static extern int msgsnd(int msqid, ref Message msgp, int msgsz, int msgflg);
-        [DllImport(DLL_NAME)] static extern int msgrcv(int msqid, ref Message msgp, int msgsz, long msgtyp, int msgflg);
-        [DllImport(DLL_NAME, EntryPoint = "my_msgctl")] static extern int msgctl(int msqid, int cmd);
-
         static void Main(string[] args)
         {
-            test_chars("test_chars");
-            var msg = new Message(1337, "test_struct");
-            test_struct(ref msg);
-
-            var result = Spock();
+            Console.WriteLine("Hello World!");
+            
+            var result = Kirk();
             Console.WriteLine($"Exit result: {result}");
         }
 
         static int Kirk()
         {
             var message = new Message(1L, "Kirk: We are attacked. Spock, send reinforcement.");
-            int msqid;
             int key = 12345;
-
-            if ((msqid = msgget(key, (int)Permissions.UserReadWrite | IPC_CREAT)) == -1)
-            {
-                Console.WriteLine("msgget: error");
-                return 1;
-            }
-
-            if (msgsnd(msqid, ref message, message.Size, 0) == -1)
-            {
-                Console.WriteLine("msgsnd: error");
-                return 1;
-            }
-
+            var queue = MessageQueue.GetOrCreate(key, Permissions.UserReadWrite);
+            queue.Send(ref message);
             Console.WriteLine("Sent");
-
             return 0;
         }
 
         static int Spock()
         {
             Message message = default;
-            int msqid;
             int key = 12345;
 
-            if ((msqid = msgget(key, (int)Permissions.UserReadWrite | IPC_CREAT)) == -1)
-            {
-                Console.WriteLine("msgget: error");
-                return 1;
-            }
+            var queue = MessageQueue.GetOrCreate(key, Permissions.UserReadWrite);
 
             for (;;)
             {
-                if (msgrcv(msqid, ref message, 200, 0, 0) == -1) // TODO: 200
-                {
-                    Console.WriteLine("msgrcv: error");
-                    return 1;
-                }
-
+                queue.Receive(ref message, 0, 0);
                 Console.WriteLine($"> {message.Text}");
             }
 
@@ -118,5 +76,65 @@ namespace NOS.Lab1
         public long Type => _type;
         public string Text => _text;
         public int Size => Encoding.Unicode.GetByteCount(Text) + 1;
+    }
+
+    public class MessageQueue : IDisposable
+    {
+        const string DLL_NAME = "../shared/msg.so";
+        const int IPC_CREAT = 0x200;
+        const int IPC_EXCL = 0x400;
+        const int IPC_NOWAIT = 0x800;
+        const int IPC_RMID = 0;
+        const int IPC_SET = 1;
+        const int IPC_STAT = 2;
+
+        public int Id { get; private init; }
+        public int Key { get; private init; }
+
+        [DllImport(DLL_NAME)] static extern int msgget(int key, int flags);
+        [DllImport(DLL_NAME)] static extern int msgsnd(int msqid, ref Message msgp, int msgsz, int msgflg);
+        [DllImport(DLL_NAME)] static extern int msgrcv(int msqid, ref Message msgp, int msgsz, long msgtyp, int msgflg);
+        [DllImport(DLL_NAME)] static extern int my_msgctl(int msqid, int cmd);
+
+        public static MessageQueue GetOrCreate(int key, Permissions permissions)
+        {
+            int id = msgget(key, (int)permissions | IPC_CREAT);
+
+            if (id == -1)
+                throw new Exception("Failed to get or create message queue.");
+            
+            return new MessageQueue
+            {
+                Id = id,
+                Key = key,
+            };
+        }
+
+        public void Send(ref Message message)
+        {
+            int result = msgsnd(Id, ref message, message.Size, 0);
+
+            if (result == -1)
+                throw new Exception("Failed to send message.");
+        }
+
+        public void Receive(ref Message message, long type, int flags)
+        {
+            int result = msgrcv(Id, ref message, Message.MaxSize, type, flags);
+
+            if (result == -1)
+                throw new Exception("Failed to receive message.");
+        }
+
+        public void Delete()
+        {
+            if (my_msgctl(Id, IPC_RMID) == -1)
+                throw new Exception("Failed to delete queue.");
+        }
+
+        public void Dispose()
+        {
+            my_msgctl(Id, IPC_RMID);
+        }
     }
 }
