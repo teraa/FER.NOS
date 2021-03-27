93d0d7e31a5d3d7ddbb149c584e85d2dbc24aeba
Tera <24725862+teraa@users.noreply.github.com>
2021-03-27 18:20:12 +0100

    update consts

--- MessageQueue/MessageQueue.cs
+++ MessageQueue/MessageQueue.cs
@@ -8,14 +8,14 @@ namespace NOS.Lab1
     public enum Permissions : int
     {
         None = 0,
-        OtherWrite = 0b_000_000_010,
-        OtherRead = 0b_000_000_100,
+        OtherWrite = 1 << 1,
+        OtherRead = 1 << 2,
         OtherReadWrite = OtherRead | OtherWrite,
-        GroupWrite = 0b_000_010_000,
-        GroupRead = 0b_000_100_000,
+        GroupWrite = 1 << 4,
+        GroupRead = 1 << 5,
         GroupReadWrite = GroupRead | GroupWrite,
-        UserWrite = 0b_010_000_000,
-        UserRead = 0b_100_000_000,
+        UserWrite = 1 << 7,
+        UserRead = 1 << 8,
         UserReadWrite = UserRead | UserWrite,
     }
 
@@ -50,14 +50,14 @@ namespace NOS.Lab1
     public class MessageQueue
     {
         const string DLL_NAME = "../shared/msg.so";
-        const int IPC_CREAT = 0x200;
-        const int IPC_EXCL = 0x400;
-        const int IPC_NOWAIT = 0x800;
-        const int IPC_RMID = 0;
-        const int IPC_SET = 1;
-        const int IPC_STAT = 2;
+        public const int IPC_CREAT = 0x200;
+        public const int IPC_EXCL = 0x400;
+        public const int IPC_NOWAIT = 0x800;
+        public const int IPC_RMID = 0;
+        public const int IPC_SET = 1;
+        public const int IPC_STAT = 2;
 
-        MessageQueue() { }
+        private MessageQueue() { }
 
         public int Id { get; private init; }
         public int Key { get; private init; }
@@ -100,6 +100,13 @@ namespace NOS.Lab1
 
             if (result == -1)
                 throw new Exception("Failed to receive message.");
+        }
+
+        public bool TryReceive(ref Message message, long type = 0, int flags = 0)
+        {
+            int result = msgrcv(Id, ref message, Message.MaxSize, type, flags);
+
+            return result != -1;
         }
 
         public void Delete()
