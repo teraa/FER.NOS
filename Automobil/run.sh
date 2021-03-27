51cf879c33b71b51e5b9e8f8937861e6cdb98556
Tera <24725862+teraa@users.noreply.github.com>
2021-03-27 22:05:47 +0100

    update scripts

--- Automobil/run.sh
+++ Automobil/run.sh
@@ -2,9 +2,8 @@
 
 trap 'kill $(jobs -p)' EXIT
 
-for i in {1..20}
-do
-    dotnet run --no-build $i $(($RANDOM % 2)) &
+for i in {1..20}; do
+    dotnet run --no-build -c Release -- $i $(($RANDOM % 2)) &
 done
 
 wait
