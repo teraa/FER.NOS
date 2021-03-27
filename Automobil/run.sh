44cd08aa9072df4649b799e53642ef5839f9d39b
Tera <24725862+teraa@users.noreply.github.com>
2021-03-27 22:32:11 +0100

    update scripts

--- Automobil/run.sh
+++ Automobil/run.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
+readonly MIN_N=5
+readonly MAX_N=100
 
+if [ $# -eq 0 ]; then
+    n=$(($RANDOM % ($MAX_N - $MIN_N) + $MIN_N))
+elif [ $# -eq 1 ]; then
+    n=$1
+else
+    echo "Usage: $0 [N]"
+    echo "    N - broj automobila"
+fi
+
+echo "Starting $n processes"
 trap 'kill $(jobs -p)' EXIT
 
-for i in {1..20}; do
-    dotnet run --no-build -c Release -- $i $(($RANDOM % 2)) &
+for i in $(seq 1 $n); do
+    ./bin/Automobil $i $(($RANDOM % 2)) &
 done
 
 wait
