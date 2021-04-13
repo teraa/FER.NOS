b85a9181f1641089d40a182a7cd778ffd73b2ae1
Tera <24725862+teraa@users.noreply.github.com>
2021-04-13 20:33:47 +0200

    add pipe example

--- Zad1b/Baza/Program.cs
+++ Zad1b/Baza/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+using System;
+using System.IO;
+using System.IO.Pipes;
 
 namespace NOS.Lab1
 {
@@ -6,7 +8,20 @@ namespace NOS.Lab1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Baza");
+
+            using var pipeServer = new NamedPipeServerStream("baza", PipeDirection.InOut);
+            pipeServer.WaitForConnection();
+            Console.WriteLine("Connected");
+
+            using var sr = new StreamReader(pipeServer);
+            string? line;
+            while ((line = sr.ReadLine()) is not null)
+            {
+                Console.WriteLine($"> {line}");
+            }
+
+            Console.WriteLine("done");
         }
     }
 }
--- Zad1b/Klijent/Program.cs
+++ Zad1b/Klijent/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+using System;
+using System.IO;
+using System.IO.Pipes;
 
 namespace NOS.Lab1
 {
@@ -6,7 +8,24 @@ namespace NOS.Lab1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Klijent");
+
+            using var pipeClient = new NamedPipeClientStream(".", "baza", PipeDirection.InOut);
+            pipeClient.Connect();
+            Console.WriteLine("Connected");
+
+            using var sw = new StreamWriter(pipeClient)
+            {
+                AutoFlush = true,
+            };
+
+            string? line;
+            while ((line = Console.ReadLine()) is not null)
+            {
+                sw.WriteLine(line);
+            }
+
+            Console.WriteLine("done");
         }
     }
 }
