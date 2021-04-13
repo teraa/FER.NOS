9a05e224081ab3385f07a33e2e3275334622d177
Tera <24725862+teraa@users.noreply.github.com>
2021-04-13 19:06:19 +0200

    add zad1b projects

---
+++ Zad1b/Baza/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NOS.Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
