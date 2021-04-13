c6fa7c03cc86c33f1e68475058723ccd2361664e
Tera <24725862+teraa@users.noreply.github.com>
2021-04-14 00:33:13 +0200

    create Proces project

---
+++ Zad1b/Proces/Program.cs
@@ -0,0 +1,46 @@
+using System;
+
+namespace NOS.Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2
+                || !int.TryParse(args[0], out var n)
+                || !int.TryParse(args[1], out var id))
+            {
+                Usage();
+                return;
+            }
+
+            if (n < 2)
+            {
+                Console.WriteLine("Error: N must be greater than 1");
+                Usage();
+                return;
+            }
+
+            if (id < 0)
+            {
+                Console.WriteLine("Error: ID must be a positive number.");
+                Usage();
+                return;
+            }
+
+            if (id >= n)
+            {
+                Console.WriteLine("Error: ID must be less than N");
+                Usage();
+                return;
+            }
+
+
+        }
+
+        static void Usage()
+        {
+            Console.WriteLine("Usage: <N> <ID>");
+        }
+    }
+}
