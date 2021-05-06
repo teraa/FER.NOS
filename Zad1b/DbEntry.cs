0a4d940e2ff255949bafa0e10d95230bb408cde0
Tera <24725862+teraa@users.noreply.github.com>
2021-05-06 02:13:57 +0200

    add DbEntry#Parse

--- Zad1b/DbEntry.cs
+++ Zad1b/DbEntry.cs
@@ -13,6 +13,16 @@ namespace NOS.Lab1.Zad1b
         public int Timestamp { get; }
         public int Count { get; }
 
+        public static DbEntry Parse(string input)
+        {
+            var parts = input.Split(',');
+            int pid = int.Parse(parts[0].Split('=')[1]);
+            int ts = int.Parse(parts[1].Split('=')[1]);
+            int count = int.Parse(parts[2].Split('=')[1]);
+
+            return new DbEntry(pid, ts, count);
+        }
+
         public override string ToString()
             => $"PId={PId},Timestamp={Timestamp},Count={Count}";
     }
